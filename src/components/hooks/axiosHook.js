98aea263333b906d7dc7f7913241f9aec7f55e74
boodah96 <2014990065@ses.yu.edu.jo>
2021-05-27 17:52:36 +0300

    custom

---
+++ src/components/hooks/axiosHook.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const useAjax = (url) => {
+    const [list, setList] = useState([]);
+
+    let config = {
+        headers: {
+            mode: 'cors',
+            cache: 'no-cache',
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const fetchingData = async(id, method = 'get', item) => {
+        if (method === 'get') {
+            const results = await axios[method](url, config);
+            setList([...results.data.results]);
+        }
+
+        if (method === 'post') {
+            item.due = new Date();
+            const results = await axios[method](url, item, config);
+            setList([...list, results.data]);
+        }
+
+        if (method === 'put') {
+            let item = list.filter((i) => i._id === id)[0] || {};
+
+            if (item._id) {
+                item.complete = !item.complete;
+                const results = await axios[method](`${url}/${id}`, item, config);
+                setList(
+                    list.map((listItem) =>
+                        listItem._id === item._id ? results.data : listItem,
+                    ),
+                );
+            }
+        }
+
+        if (method === 'delete') {
+            let item = list.find((i) => i._id === id) || {};
+
+            if (item._id) {
+                const results = await axios[method](`${url}/${id}`, config);
+                setList(list.filter((listItem) => listItem._id !== results.data._id));
+            }
+        }
+    };
+
+    return [list, fetchingData];
+};
+
+export default useAjax;
